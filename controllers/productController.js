20719f594aac5c726727330769a5a49685baffa8
Nguyen Tuan Kiet <kiet.ngt@kyanon.digital>
2021-11-21 22:21:55 +0700

    Update code

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,22 @@
 var productServices = require("../services/productServices");
-var productModel = require ("../models/productModel");
-var imageModel=require ("../models/imageModel");
+var productModel = require("../models/productModel");
+var imageModel = require("../models/imageModel");
 exports.getListProduct = async function getListProduct() {
   return await productServices.getListProduct();
 };
 exports.getListLikeProduct = async function getListLikeProduct() {
   return await productServices.getListLikeProduct();
 };
-exports.getListProductByCategory = async function getListProductByCategory(categorys) {
+exports.getListProductByCategory = async function getListProductByCategory(
+  categorys
+) {
   return await productServices.getListProductByCategory(categorys);
 };
 
-exports.getListProductByIdCategorys = async function getListProductByIdCategorys(id) {
-  return await productServices.getListProductByIdCategorys(id);
-};
+exports.getListProductByIdCategorys =
+  async function getListProductByIdCategorys(id) {
+    return await productServices.getListProductByIdCategorys(id);
+  };
 exports.getProductById = async function getProductById(id) {
   return await productServices.getProductById(id);
 };
@@ -24,7 +27,21 @@ exports.addNew = async function addNewProduct(params) {
 };
 
 exports.edit = async function editProduct(id, params) {
-  let { nameProduct, price_product, id_category, stock, avg_vote , description_product ,quantity_product,nameImage,rom_product,ram_product ,camera_late_product,pin_product} = params;
+  let {
+    nameProduct,
+    price_product,
+    id_category,
+    stock,
+    chip_product,
+    avg_vote,
+    description_product,
+    quantity_product,
+    nameImage,
+    rom_product,
+    ram_product,
+    camera_late_product,
+    pin_product,
+  } = params;
   let products = {
     id,
     nameProduct,
@@ -35,10 +52,11 @@ exports.edit = async function editProduct(id, params) {
     stock,
     avg_vote,
     nameImage,
+    chip_product,
     ram_product,
     rom_product,
     camera_late_product,
-    pin_product
+    pin_product,
   };
   return await productServices.edit(products);
 };
@@ -48,10 +66,9 @@ exports.remove = async function removeProductById(id) {
 };
 
 exports.addImage = async function addImageProduct(nameImage) {
- 
   const modelImage = new imageModel({
-    nameImage:nameImage,
-  })
+    nameImage: nameImage,
+  });
   // Tạo mới một sản phẩm
   return await productServices.addImage(modelImage);
 };
