e4b0d06ae09ae1d71bd1f937969bdb4e01fa28b9
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-11-25 15:55:41 +0700

    update

--- routes/user/notification.js
+++ routes/user/notification.js
@@ -13,7 +13,7 @@ router.get("/:idUser", middle , async function (req, res, next) {
   
   const {idUser} =  req.params;
   if(idUser!==null){
-    const noti = await notificationModel.find({id_user:idUser}).populate('id_billdetail');
+    const noti = await notificationModel.find({id_user:idUser}).populate({path:'id_billdetail',populate :{path : 'products.id_product'}}) ;
     res.status(200).json({status:1,data:noti});
   }else{
     res.status(200).json({status:-1,error:'Không tìm thấy dữ liệu'});
