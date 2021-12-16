999d13c059a2fb0bdd471a837c5a372e2fb29281
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-12-17 00:40:01 +0700

    update

--- models/otpModel.js
+++ models/otpModel.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
-const OtpSchema = new Schema({
-  email: { type: String, require: true },
-  otp: { type: String, require: true },
-  expire_at: { type: Date, default: Date.now, expires: 3000 },
-});
-
+const OtpSchema = new Schema(
+  {
+    email: { type: String, require: true },
+    otp: { type: String, require: true },
+  },
+  { timestamps: true }
+);
+OtpSchema.index({ createdAt: 1 }, { expireAfterSeconds: 180 });
 module.exports = mongoose.model("Otp", OtpSchema);
