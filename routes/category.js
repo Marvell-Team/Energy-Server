02631abae1f3f3267d46b2d74f12124b4eb9327e
Vũ Thành Nam <71971442+namsdai1@users.noreply.github.com>
2021-12-18 20:25:59 +0700

    udpa

--- routes/category.js
+++ routes/category.js
@@ -7,35 +7,44 @@ const categoryModel = require("../models/categoryModel");
 //     // let list = await categoryCotroller.getListCategories();
 //     res.render("new-category");
 //   });
-  router.get("/:categorys", async  (req, res, next) => {
-    let { categorys } = req.params;
-    let users = await categoriesController.getListCategoriesbyCategorys(categorys);
-    res.status(200).json(users);
-  });
+router.get("/:categorys", async (req, res, next) => {
+  let { categorys } = req.params;
+  let users = await categoriesController.getListCategoriesbyCategorys(
+    categorys
+  );
+  res.status(200).json(users);
+});
 router.get("/add-category", async function (req, res, next) {
-    let categories = await categoryCotroller.getListCategories();
-    res.render("new-category", { categories, title: "Add Category" });
-  });
-  
-  router.post("/add-category", async function (req, res, next) {
-    // req.body = {...req.body, avatar: 'assets/images/' + req.file.originalname}
-    let { body } = req;
-    
-    await categoryCotroller.addCategory(body, res);
-    // res.redirect("/category");
-  });
-  router.post("/add", async  (req, res, next) => {
-    let { body } = req;
-    const {name_category,categorys,image}=body;
-    const cate = new categoryModel({
-      name_category:name_category,
-      categorys:categorys,
-      img_categorys:image,
-
-    }) 
-    const aa=await cate.save();
-    res.status(200).json({status:1,data:aa});
-  
+  let categories = await categoryCotroller.getListCategories();
+  res.render("new-category", { categories, title: "Add Category" });
 });
 
-  module.exports = router;
+router.post("/add-category", async function (req, res, next) {
+  // req.body = {...req.body, avatar: 'assets/images/' + req.file.originalname}
+  let { body } = req;
+
+  await categoryCotroller.addCategory(body, res);
+  // res.redirect("/category");
+});
+router.post("/add", async (req, res, next) => {
+  let { body } = req;
+  const { name_category, categorys, image } = body;
+  const cate = new categoryModel({
+    name_category: name_category,
+    categorys: categorys,
+    img_categorys: image,
+  });
+  const aa = await cate.save();
+  res.status(200).json({ status: 1, data: aa });
+});
+router.get("/getOne/:id", async (req, res, next) => {
+  let { id } = req.params;
+  const cate = await categoryModel.findById(id);
+  if (cate) {
+    res.status(200).json({ status: 1, data: cate });
+  } else {
+    res.status(404).json({ status: -1, data: "Not found" });
+  }
+});
+
+module.exports = router;
